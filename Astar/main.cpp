e38ee886c0f6a1fcd4b2d3fd379283c63c9e7020
ZyvvL <njfuzy@126.com>
2017-07-09 10:11:57 +0800

    push a 3D A*

--- Astar/main.cpp
+++ Astar/main.cpp
@@ -15,15 +15,19 @@ int main()
         {200,  0,200,  0,  0,  0,  0,200,  0,  0,  0,200},
         {200,200,200,200,200,200,200,200,200,200,200,200}
     };
+
+    ZY_UINT32 MapSize_X = maze.size();
+    ZY_UINT32 MapSize_Y = maze[0].size();
+
     Astar astar;
-    vector<vector<MapPoint>> maze1(8);
-    for(int i=0;i<8;i++) {
-         maze1[i].resize(12);
+    vector<vector<MapPoint>> maze1(MapSize_X);
+    for(int i=0;i<MapSize_X;i++) {
+         maze1[i].resize(MapSize_Y);
      }
 
     //初始化地图
-    for(int i =0; i< 8;i++)
-        for(int j=0; j< 12;j++)
+    for(int i =0; i< MapSize_X;i++)
+        for(int j=0; j< MapSize_Y;j++)
         {
             MapPoint map_point;
             if(maze[i][j] == 200)
@@ -33,7 +37,7 @@ int main()
             maze1[i][j] = map_point;
         }
 
-    //设置时间占用点
+    //设置出入口和时间占用点
     (maze1[1][2]).OccupyTime_set.insert(2);
     (maze1[2][2]).OccupyTime_set.insert(1);
     (maze1[4][10]).PointType =2;
